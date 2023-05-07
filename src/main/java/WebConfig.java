2c827948f401103474735cb2f7a0b70f544467da
Flor <florvega1598@gmail.com>
2023-05-07 11:00:52 -0300

    Corrección CORS

--- src/main/java/WebConfig.java
+++ src/main/java/WebConfig.java
@@ -1,5 +1,4 @@
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,9 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @EnableWebMvc
-
 @Configuration
-
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
