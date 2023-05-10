60deb19250152325f40c8a80392aef2f1336f951
Flor <florvega1598@gmail.com>
2023-05-10 10:06:48 -0300

    cors

--- src/main/java/com/PortafolioMFV/mfv/Controller/Chys.java
+++ src/main/java/com/PortafolioMFV/mfv/Controller/Chys.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("skills")
-@CrossOrigin(origins = "https://mfvportfolio.web.app")
+
 public class Chys {
     @Autowired
     Shys shys;
--- src/main/java/com/PortafolioMFV/mfv/MfvApplication.java
+++ src/main/java/com/PortafolioMFV/mfv/MfvApplication.java
@@ -2,6 +2,10 @@ package com.PortafolioMFV.mfv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class MfvApplication {
@@ -10,6 +14,15 @@ public class MfvApplication {
         SpringApplication.run(MfvApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://mfvportfolio.web.app/", "http://localhost:4200")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }
-
-                
